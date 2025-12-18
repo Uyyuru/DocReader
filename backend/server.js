10bb702ff92bfa0bb39ec7e377eb8fad5a4ec1fa
saitej <uyyuruhimasaitej@gmail.com>
2025-12-18 22:12:43 +0530

    added frontend url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());  
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://doc-reader-psi.vercel.app",
   credentials: true
 }));
 
